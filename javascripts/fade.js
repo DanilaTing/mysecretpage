ca12a42d960e70210e129f713c4b09bd648728ed
DanilaTing <tinggone@gmail.com>
2017-11-10 17:51:37 +0300

    mobile again

--- javascripts/fade.js
+++ javascripts/fade.js
@@ -1,50 +1,53 @@
 $(function () {
 
-  $(function () {
-    $('#obj1').css({'opacity': '1', 'transition': '2s'})
+  if ($(window).width() > 768) {
+    $(function () {
+      $('#obj1').css({'opacity': '1', 'transition': '2s'})
 
-    $(function() {
-      $(window).on('scroll', function() {
-        console.log('gogogo')
-        var scrollTop = $(this).scrollTop()
-        console.log(scrollTop)
+      $(function() {
+        $(window).on('scroll', function() {
+          console.log('gogogo')
+          var scrollTop = $(this).scrollTop()
+          console.log(scrollTop)
 
-        if (scrollTop > 50) {
-          $('.scroll').css({'opacity': '0', 'transition': '1s'})
-        } else {
-          $('.scroll').css({'opacity': '1', 'transition': '1s'})
-        }
+          if (scrollTop > 50) {
+            $('.scroll').css({'opacity': '0', 'transition': '1s'})
+          } else {
+            $('.scroll').css({'opacity': '1', 'transition': '1s'})
+          }
 
-        if (scrollTop > 70) {
-          $('#obj2').css({'opacity': '1', 'transition': '1s'})
-        } else {
-          $('#obj2').css({'opacity': '0', 'transition': '1s'})
-        }
+          if (scrollTop > 70) {
+            $('#obj2').css({'opacity': '1', 'transition': '1s'})
+          } else {
+            $('#obj2').css({'opacity': '0', 'transition': '1s'})
+          }
 
-        if (scrollTop > 100) {
-          $('#obj3').css({'filter': 'blur(0px)', 'transition': '1s'})
-        } else {
-          $('#obj3').css({'filter': 'blur(10px)', 'transition': '1s'})
-        }
+          if (scrollTop > 100) {
+            $('#obj3').css({'filter': 'blur(0px)', 'transition': '1s'})
+          } else {
+            $('#obj3').css({'filter': 'blur(10px)', 'transition': '1s'})
+          }
 
-        if (scrollTop > 350) {
-          $('#obj4').css({'opacity': '1', 'transition': '1s'})
-        } else {
-          $('#obj4').css({'opacity': '0', 'transition': '1s'})
-        }
+          if (scrollTop > 350) {
+            $('#obj4').css({'opacity': '1', 'transition': '1s'})
+          } else {
+            $('#obj4').css({'opacity': '0', 'transition': '1s'})
+          }
 
-        if (scrollTop > 350) {
-          $('#obj5').css({'filter': 'blur(0px)', 'transition': '1s'})
-        } else {
-          $('#obj5').css({'filter': 'blur(10px)', 'transition': '1s'})
-        }
+          if (scrollTop > 350) {
+            $('#obj5').css({'filter': 'blur(0px)', 'transition': '1s'})
+          } else {
+            $('#obj5').css({'filter': 'blur(10px)', 'transition': '1s'})
+          }
 
-        if (scrollTop > 400) {
-          $('#obj6').css({'opacity': '1', 'transition': '1s'})
-        } else {
-          $('#obj6').css({'opacity': '0', 'transition': '1s'})
-        }
+          if (scrollTop > 400) {
+            $('#obj6').css({'opacity': '1', 'transition': '1s'})
+          } else {
+            $('#obj6').css({'opacity': '0', 'transition': '1s'})
+          }
+        })
       })
     })
-  })
+  }
+
 })
