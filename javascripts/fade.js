6d80a2b719e38fd20e61b3d1ff0f7e08a79c33d3
DanilaTing <tinggone@gmail.com>
2017-11-10 17:21:55 +0300

    scroll down fix

--- javascripts/fade.js
+++ javascripts/fade.js
@@ -10,39 +10,39 @@ $(function () {
         console.log(scrollTop)
 
         if (scrollTop > 50) {
-          $('.scroll').css({'opacity': '0', 'transition': '2s'})
+          $('.scroll').css({'opacity': '0', 'transition': '1s'})
         } else {
-          $('.scroll').css({'opacity': '1', 'transition': '2s'})
+          $('.scroll').css({'opacity': '1', 'transition': '1s'})
         }
 
         if (scrollTop > 70) {
-          $('#obj2').css({'opacity': '1', 'transition': '2s'})
+          $('#obj2').css({'opacity': '1', 'transition': '1s'})
         } else {
-          $('#obj2').css({'opacity': '0', 'transition': '2s'})
+          $('#obj2').css({'opacity': '0', 'transition': '1s'})
         }
 
         if (scrollTop > 100) {
-          $('#obj3').css({'filter': 'blur(0px)', 'transition': '3s'})
+          $('#obj3').css({'filter': 'blur(0px)', 'transition': '1s'})
         } else {
-          $('#obj3').css({'filter': 'blur(10px)', 'transition': '3s'})
+          $('#obj3').css({'filter': 'blur(10px)', 'transition': '1s'})
         }
 
         if (scrollTop > 350) {
-          $('#obj4').css({'opacity': '1', 'transition': '2s'})
+          $('#obj4').css({'opacity': '1', 'transition': '1s'})
         } else {
-          $('#obj4').css({'opacity': '0', 'transition': '2s'})
+          $('#obj4').css({'opacity': '0', 'transition': '1s'})
         }
 
         if (scrollTop > 350) {
-          $('#obj5').css({'filter': 'blur(0px)', 'transition': '3s'})
+          $('#obj5').css({'filter': 'blur(0px)', 'transition': '1s'})
         } else {
-          $('#obj5').css({'filter': 'blur(10px)', 'transition': '3s'})
+          $('#obj5').css({'filter': 'blur(10px)', 'transition': '1s'})
         }
 
         if (scrollTop > 400) {
-          $('#obj6').css({'opacity': '1', 'transition': '2s'})
+          $('#obj6').css({'opacity': '1', 'transition': '1s'})
         } else {
-          $('#obj6').css({'opacity': '0', 'transition': '2s'})
+          $('#obj6').css({'opacity': '0', 'transition': '1s'})
         }
       })
     })
