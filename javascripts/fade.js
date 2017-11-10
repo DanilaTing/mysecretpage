7958f8465bdab64c87c3f60052c803a49b508473
DanilaTing <tinggone@gmail.com>
2017-11-10 22:21:17 +0300

    mobile working ok

--- javascripts/fade.js
+++ javascripts/fade.js
@@ -1,53 +1,74 @@
 $(function () {
 
-  if ($(window).width() > 768) {
-    $(function () {
-      $('#obj1').css({'opacity': '1', 'transition': '2s'})
+  // var desktop = $(window).width() > 768
+  // var mobile = $(window).width() <= 768
+  var mobile = $('.mobileCheck').is(':visible');
+  console.log(mobile);
 
-      $(function() {
-        $(window).on('scroll', function() {
-          console.log('gogogo')
-          var scrollTop = $(this).scrollTop()
-          console.log(scrollTop)
+  if (!mobile) {
+    $('#obj1').css({'opacity': '0', 'transition': '0s'})
+    $('#obj2').css({'opacity': '0', 'transition': '0s'})
+    $('#obj4').css({'opacity': '0', 'transition': '0s'})
+    $('#obj6').css({'opacity': '0', 'transition': '0s'})
 
-          if (scrollTop > 50) {
-            $('.scroll').css({'opacity': '0', 'transition': '1s'})
-          } else {
-            $('.scroll').css({'opacity': '1', 'transition': '1s'})
-          }
+    $('#obj3').css({'filter': 'blur(0px)', 'transition': '0s'})
+    $('#obj3').css({'filter': 'blur(10px)', 'transition': '0s'})
+    $('#obj5').css({'filter': 'blur(0px)', 'transition': '0s'})
+    $('#obj5').css({'filter': 'blur(10px)', 'transition': '0s'})
+  }
 
-          if (scrollTop > 70) {
-            $('#obj2').css({'opacity': '1', 'transition': '1s'})
-          } else {
-            $('#obj2').css({'opacity': '0', 'transition': '1s'})
-          }
+  function fade () {
+    $('#obj1').css({'opacity': '1', 'transition': '2s'})
 
-          if (scrollTop > 100) {
-            $('#obj3').css({'filter': 'blur(0px)', 'transition': '1s'})
-          } else {
-            $('#obj3').css({'filter': 'blur(10px)', 'transition': '1s'})
-          }
+    $(function() {
+      $(window).on('scroll', function() {
+        console.log('gogogo')
+        var scrollTop = $(this).scrollTop()
+        console.log(scrollTop)
 
-          if (scrollTop > 350) {
-            $('#obj4').css({'opacity': '1', 'transition': '1s'})
-          } else {
-            $('#obj4').css({'opacity': '0', 'transition': '1s'})
-          }
+        if (scrollTop > 50) {
+          $('.scroll').css({'opacity': '0', 'transition': '1s'})
+        } else {
+          $('.scroll').css({'opacity': '1', 'transition': '1s'})
+        }
 
-          if (scrollTop > 350) {
-            $('#obj5').css({'filter': 'blur(0px)', 'transition': '1s'})
-          } else {
-            $('#obj5').css({'filter': 'blur(10px)', 'transition': '1s'})
-          }
+        if (scrollTop > 70) {
+          $('#obj2').css({'opacity': '1', 'transition': '1s'})
+        } else {
+          $('#obj2').css({'opacity': '0', 'transition': '1s'})
+        }
 
-          if (scrollTop > 400) {
-            $('#obj6').css({'opacity': '1', 'transition': '1s'})
-          } else {
-            $('#obj6').css({'opacity': '0', 'transition': '1s'})
-          }
-        })
+        if (scrollTop > 100) {
+          $('#obj3').css({'filter': 'blur(0px)', 'transition': '1s'})
+        } else {
+          $('#obj3').css({'filter': 'blur(10px)', 'transition': '1s'})
+        }
+
+        if (scrollTop > 350) {
+          $('#obj4').css({'opacity': '1', 'transition': '1s'})
+        } else {
+          $('#obj4').css({'opacity': '0', 'transition': '1s'})
+        }
+
+        if (scrollTop > 350) {
+          $('#obj5').css({'filter': 'blur(0px)', 'transition': '1s'})
+        } else {
+          $('#obj5').css({'filter': 'blur(10px)', 'transition': '1s'})
+        }
+
+        if (scrollTop > 400) {
+          $('#obj6').css({'opacity': '1', 'transition': '1s'})
+        } else {
+          $('#obj6').css({'opacity': '0', 'transition': '1s'})
+        }
       })
     })
   }
 
+  if (!mobile) {
+    console.log('desktop');
+    fade ();
+  } else {
+    console.log('mobile');
+  }
 })
